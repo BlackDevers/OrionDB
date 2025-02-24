ea98be35fd18b26f30eab3baae18343f19badb66
borz7zy <borz7zy@yandex.ru>
2025-02-24 23:39:36 +0100

    code recactor

--- compability/c++/websocket_orion.cpp
+++ compability/c++/websocket_orion.cpp
@@ -1,57 +1,201 @@
+/**
+ * @ENCODING: UTF-8
+ *
+ * @UNIX_COMPILATION_COMMAND: g++ -std=c++17 -o websocket_orion websocket_orion.cpp -lpthread
+ * @WINDOWS_COMPILATION_COMMAND: cl /std:c++17 /EHsc /Fe:websocket_client.exe websocket_client.cpp ws2_32.lib
+ *
+ * @AUTHORS: jxbc (original client), borz7zy (code refactor)
+ * @DATE: 02-24-2025
+ *
+ * @LICENSE: MIT
+ */
+
 #include <iostream>
-#include <websocketpp/config/asio_no_tls_client.hpp>
-#include <websocketpp/client.hpp>
+#include <string>
+#include <thread>
+#include <vector>
 #include <nlohmann/json.hpp>
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#endif
+
 using json = nlohmann::json;
-typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
-
-void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
-    try {
-        json response = json::parse(msg->get_payload());
-        std::cout << "Received: " << response.dump(4) << std::endl;
-    } catch (const std::exception& e) {
-        std::cerr << "Error parsing JSON: " << e.what() << std::endl;
+class WebSocketClient
+{
+public:
+    WebSocketClient(const std::string &host,
+                    const std::string &port,
+                    const std::string &path) : host(host),
+                                               port(port),
+                                               path(path),
+                                               running(false)
+    {
+#ifdef _WIN32
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
+#endif
     }
-}
 
-void insertMany(client* c, websocketpp::connection_hdl hdl) {
+    ~WebSocketClient()
+    {
+        closeSocket();
+#ifdef _WIN32
+        WSACleanup();
+#endif
+    }
+
+    bool connect()
+    {
+        struct addrinfo hints{}, *res;
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+
+        if (getaddrinfo(host.c_str(), port.c_str(), &hints, &res) != 0)
+        {
+            std::cerr << "Error: getaddrinfo failed" << std::endl;
+            return false;
+        }
+
+        sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (sockfd == -1)
+        {
+            std::cerr << "Error: Unable to create socket" << std::endl;
+            freeaddrinfo(res);
+            return false;
+        }
+
+        if (::connect(sockfd, res->ai_addr, res->ai_addrlen) == -1)
+        {
+            std::cerr << "Error: Connection failed" << std::endl;
+            closeSocket();
+            freeaddrinfo(res);
+            return false;
+        }
+        freeaddrinfo(res);
+
+        if (handshake())
+        {
+            running = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    void sendMessage(const json &message)
+    {
+        if (!running || sockfd == -1)
+        {
+            std::cerr << "Error: Attempt to send on a closed socket" << std::endl;
+            return;
+        }
+
+        std::string payload = message.dump();
+        std::string frame = encodeFrame(payload);
+        if (send(sockfd, frame.c_str(), frame.size(), 0) == -1)
+        {
+            std::cerr << "Error: Failed to send data" << std::endl;
+            closeSocket();
+            running = false;
+        }
+    }
+
+    void receiveMessages()
+    {
+        while (running)
+        {
+            char buffer[1024] = {0};
+            int bytesReceived = recv(sockfd, buffer, sizeof(buffer), 0);
+
+            if (bytesReceived <= 0)
+            {
+                std::cerr << "Connection closed by server" << std::endl;
+                closeSocket();
+                running = false;
+                break;
+            }
+
+            std::string response = decodeFrame(std::string(buffer, bytesReceived));
+            std::cout << "Received: " << response << std::endl;
+        }
+    }
+
+private:
+    std::string host, port, path;
+    bool running;
+    int sockfd;
+#ifdef _WIN32
+    WSADATA wsaData;
+#endif
+
+    void closeSocket()
+    {
+        if (sockfd != -1)
+        {
+#ifdef _WIN32
+            closesocket(sockfd);
+#else
+            close(sockfd);
+#endif
+            sockfd = -1;
+            running = false;
+        }
+    }
+
+    bool handshake()
+    {
+        std::string request = "GET " + path + " HTTP/1.1\r\n";
+        request += "Host: " + host + ":" + port + "\r\n";
+        request += "Upgrade: websocket\r\n";
+        request += "Connection: Upgrade\r\n";
+        request += "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
+        request += "Sec-WebSocket-Version: 13\r\n\r\n";
+
+        send(sockfd, request.c_str(), request.size(), 0);
+
+        char buffer[1024] = {0};
+        recv(sockfd, buffer, sizeof(buffer), 0);
+        return std::string(buffer).find("101") != std::string::npos;
+    }
+
+    std::string encodeFrame(const std::string &message)
+    {
+        std::string frame;
+        frame.push_back(0x81);
+        frame.push_back(message.size());
+        frame.append(message);
+        return frame;
+    }
+
+    std::string decodeFrame(const std::string &frame)
+    {
+        return frame.substr(2);
+    }
+};
+
+int main()
+{
+    WebSocketClient client("localhost", "5665", "/test_db@12345/users");
+    if (!client.connect())
+        return -1;
+
+    std::thread receiver(&WebSocketClient::receiveMessages, &client);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
     json req = {
         {"method", "insertMany"},
-        {"value", { { {"id", 1}, {"test", "test"}, {"cool", 1} }, 
-                     { {"id", 2}, {"test", "test2"}, {"cool", 2} } }}
-    };
-    
-    std::string message = req.dump();
-    c->send(hdl, message, websocketpp::frame::opcode::text);
-}
+        {"value", {{{"id", 1}, {"test", "test"}, {"cool", 1}}, {{"id", 2}, {"test", "test2"}, {"cool", 2}}}}};
+    client.sendMessage(req);
 
-void run() {
-    client c;
-    c.init_asio();
-    c.set_message_handler(std::bind(&on_message, &c, std::placeholders::_1, std::placeholders::_2));
-    
-    websocketpp::lib::error_code ec;
-    client::connection_ptr con = c.get_connection("ws://localhost:5665/test_db@12345/users", ec);
-    if (ec) {
-        std::cerr << "Connection failed: " << ec.message() << std::endl;
-        return;
-    }
-    
-    websocketpp::connection_hdl hdl = con->get_handle();
-    c.connect(con);
-    
-    std::thread([&c, hdl]() {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        insertMany(&c, hdl);
-    }).detach();
-    
-    c.run();
-}
-
-int main() {
-    run();
+    receiver.join();
     return 0;
 }
